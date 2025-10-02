aec58bcef752f2fd4874e9cbeb39623cb51b40f1
Ethanpanda90 <ethanpanda90@gmail.com>
2025-10-02 01:05:14 +0000

    done turtle tricks

--- lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
@@ -15,6 +15,15 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 # Make each side of the pentagon a different color with 
 # tina.pencolor()
 
-... # Your code here
+tina.forward(100)
+tina.left(72)
+tina.forward(100)
+tina.left(72)
+tina.forward(100)
+tina.left(72)
+tina.forward(100)
+tina.left(72)
+tina.forward(100)
+tina.left(72) # Your code here
 
 turtle.exitonclick()                    # Close the window when we click on it
