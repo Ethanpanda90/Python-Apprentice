81f03a6923faa39d7bcd2ed5d3ad3431f79437f4
Ethanpanda90 <ethanpanda90@gmail.com>
2025-11-27 02:25:34 +0000

    TASH ME IDIOT AHAHHAHAA!

--- lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
+++ lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
@@ -5,4 +5,51 @@
 #
 # Hint: See 08a_More Turtle Programs, section 'Click on the Screen'
  
-... # Your code here
+import turtle as turtle
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('white')
+
+t = turtle.Turtle()
+t.penup
+
+def set_turtle_image(turtle, image_name):
+    from pathlib import Path
+    image_dir = Path(__file__).parent.parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
+
+    image_dir = Path(__file__).parent.parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+set_background_image(screen, 'emoji2.png')
+
+t.goto(0,-100)
+set_turtle_image(t,'moustache3.gif')
+
+def screen_clicked(x, y):
+    """Print the x and y coordinates of the screen when clicked.
+    and make the turtle move to the clicked location."""
+
+    print('You pressed: x=' + str(x) + ', y=' + str(y))
+
+    t.goto(x, y)
+
+screen.onclick(screen_clicked)
+
+turtle.done() 
