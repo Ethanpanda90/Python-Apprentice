3942bf61bf18b7786f428a5762d75b0bde99b643
Ethanpanda90 <ethanpanda90@gmail.com>
2026-01-08 02:26:45 +0000

    making crazy loops?

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -5,25 +5,70 @@ Make your own crazy spiral with a pattern like
 in 14_FLaming_Ninja_Star.py, but use what you've learned about loops
 """
 
-... # Copy code to make a turtle and set up the window
+import random
+import turtle
 
-t = ... # Create a turtle named t
 
-# 1) Complete make_a_shape() to make the turtle move in some pattern. 
-# For instance, you can make it go left 30 degrees, then forward 50 pixels, 
-# then right 60 degrees, then forward 100 pixels. Make any shape you like.
+# Returns a random color!
+def getRandomColor():
+    return "#%06X" % (random.randint(0, 0xFFFFFF))
 
-def make_a_shape(t):
-    """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
+
+colors = ["red", "blue", "green", "yellow", "orange"]
+
+
+def getNextColor(i):
+    return colors[i % len(colors)]
+
+turtle.setup(600,600,0,0)               # Set the size of the window
+window = turtle.Screen()
+
+
+t = turtle.Turtle() 
+
+t.shape("turtle") 
+
+t.width(2) 
+
+t.speed(0) 
+
+def make_a_hexagon(t):
+    for i in range(3):
+        t.pencolor('Black')
+        t.forward(100)
+        t.left(60)
+        t.pencolor(getRandomColor())
+        t.forward(100)
+        t.left(60)
+        
+def make_a_circle(t):
+    for i in range(360):
+        t.pencolor('Black')
+        t.forward(60)
+        t.left(1)
 
 # 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
 # For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
 # The second ... in the for loop should be the number of shapes you want to make, 
 # for example 100, or it could use islice(), cycle(), or a list of numbers.
 
-num_shapes = ...
+num_shapes = 67
 
-for i in range(...):
-    make_a_shape(t)
+for i in range(36):
+    t.fillcolor('Black')
+    t.begin_fill()
+    make_a_hexagon(t)
     t.right(360/num_shapes)
+    t.end_fill
+    t.fillcolor(getRandomColor())
+    t.begin_fill()
+    make_a_hexagon(t)
+    t.right(360/num_shapes)
+    t.end_fill()
+
+for i in range(120):
+    turtle.fillcolor('Black')
+    make_a_circle(t)
+
+turtle.hideturtle()
+turtle.done()
