50bc4bdc5cc79bb16f2e9904d7e24bcba3bfad2e
Ethanpanda90 <ethanpanda90@gmail.com>
2026-03-27 00:51:41 +0000

    MY ARMS...

--- lessons/30_Loops/070_List_Story.py
+++ lessons/30_Loops/070_List_Story.py
@@ -12,8 +12,8 @@ words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 'tim
 
 # Create a story using the words in the list
 
-story = words[0], words[2]
+story = words[0], words[2], words[7], words[9], words[1], words[14], words[13], words[5], words[12], words[14], words[3], words[10], words[15], words[10], words[17], words[18]
 
 
 
-print(story)
+print(' '.join(story))
